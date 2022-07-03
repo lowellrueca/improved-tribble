25e0c4a4821124468c6d71e2c739953430461a90
lowellrueca <lowellrueca@gmail.com>
2022-07-03 19:08:54 +0800

    creating models.py file

---
+++ api/db/models.py
@@ -0,0 +1,20 @@
+""" Contains database models """
+
+from tortoise.fields import CharField, UUIDField, DecimalField
+from tortoise.models import Model
+
+
+class AbstractModel(Model):
+    id = UUIDField(pk=True)
+
+    class Meta:
+        abstract = True
+
+
+class Product(AbstractModel):
+    """ Db model for products """
+    name = CharField(max_length=128, nullable=False)
+    price = DecimalField(max_digits=8, decimal_places=2)
+
+    class Meta: 
+        table = "desktop"
