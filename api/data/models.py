3fe8576bd430713e94d7629fbaf8aa7faaab4eaf
lowellrueca <lowellrueca@gmail.com>
2022-07-11 22:15:54 +0800

    import field and creating ProductAttributeModel class

--- api/data/models.py
+++ api/data/models.py
@@ -1,5 +1,12 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from decimal import Decimal
 from typing import Any, Dict
+
+
+@dataclass
+class ProductAttributeModel:
+    name: str = field(default_factory=str, init=True)
+    price: Decimal = field(default_factory=Decimal, init=True)
 
 
 @dataclass
