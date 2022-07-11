1c8d9b85efb502211bc66f8c1269f9e29e9752a6
lowellrueca <lowellrueca@gmail.com>
2022-07-11 22:16:43 +0800

    importing ProductAttributeModelClass and validate_data function

--- api/data/__init__.py
+++ api/data/__init__.py
@@ -1,5 +1,6 @@
-from .models import DataModel
+from .models import DataModel, ProductAttributeModel
 from .repository import Repository, use_repository
+from .validations import validate_data
 
 
-__all__ = ["DataModel", "Repository", "use_repository"]
+__all__ = ["DataModel", "ProductAttributeModel", "Repository", "use_repository", "validate_data"]
