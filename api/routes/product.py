6feb6620cc2684f7e6d210c84165024a48ab05bc
lowellrueca <lowellrueca@gmail.com>
2022-07-16 23:48:41 +0800

    rewrite with changed use_repository implementaion

--- api/routes/product.py
+++ api/routes/product.py
@@ -1,106 +1,110 @@
 import logging
-from typing import Any, Dict, List
+from typing import List
 from uuid import UUID
 
 from starlette.exceptions import HTTPException
 from starlette.requests import Request
 from starlette.responses import JSONResponse, Response
 from starlette.routing import Route
-from tortoise.exceptions import DoesNotExist, IntegrityError, ValidationError
+from tortoise.exceptions import DoesNotExist
 from tortoise.models import Model
-from tortoise.queryset import QuerySet
 
-from ..data import ProductAttributeModel, Repository
-from ..data import use_repository, validate_data
+from ..data import ProductRepository, ProductSchema
+from ..data import use_repository
 from ..db import Product
 
 logger = logging.getLogger("root")
 
 
-@use_repository(model=Product)
+@use_repository(repository=ProductRepository)
 async def get_products(
         request: Request, 
-        repository:Repository
+        repository: ProductRepository
     ) -> JSONResponse:
 
-    content = List[Dict[str, Any]]
+    global content
     try:
-        query_set: QuerySet[Product] = await repository.all()
-        content = await repository.serialize_query_set(query_set=query_set)
+        models: List[Product] = await repository.all()
+        content = ProductSchema().dump(obj=models, many=True)
     
     except Exception as err:
-        logger.exception(err)
+        logger.exception(err.args)
 
     return JSONResponse(content=content)
 
-@use_repository(model=Product)
-async def get_by_id(
+@use_repository(repository=ProductRepository)
+async def get_product_by_id(
         request: Request, 
-        repository:Repository
+        repository: ProductRepository
     ) -> JSONResponse:
 
+    global content
     try:
-        query_set_single = await repository.get(param=request.path_params)
-        content = await repository.serialize_query_set_single(
-            query_set_single=query_set_single) 
+        model = await repository.get(params=request.path_params)
+        content = ProductSchema().dump(obj=model)
 
-    except (Exception, DoesNotExist) as err:
+    except Exception as err:
+        if isinstance(err, DoesNotExist):
+            raise HTTPException(status_code=404, detail="Product does not exist")
+
         logger.exception(err)
-        raise HTTPException(status_code=404, detail="Product does not exist")
 
     return JSONResponse(content=content)
 
-@validate_data(attribute_model=ProductAttributeModel)
-@use_repository(model=Product)
+@use_repository(repository=ProductRepository)
 async def create_product(
         request: Request, 
-        repository: Repository, 
-        params: Dict[str, Any]
+        repository: ProductRepository, 
     ) -> JSONResponse:
 
     try:
+        payload = await request.json()
+        schema: ProductSchema = ProductSchema()
+        schema.load(payload)
+        params = payload["data"]["attributes"]
+
         model: Model | Product = await repository.create(params=params)
-        result: Dict[str, Any] = await repository.serialize_model(model=model)
+        content = schema.dump(obj=model)
     
-    except (IntegrityError, ValidationError) as err:
-        if isinstance(err, IntegrityError):
-            err_args = getattr(err, "args")[0]
-            detail = getattr(err_args, "args")[0] 
-            raise HTTPException(status_code=422, detail=detail)
+    except Exception as err:
+        logger.exception(msg=err)
+        raise HTTPException(status_code=422, detail=str(err))
 
-        err_args = getattr(err, "args")[0]
-        raise HTTPException(status_code=422, detail=f"{err_args}")
+    return JSONResponse(content=content, status_code=201)
 
-    return JSONResponse(content=result, status_code=201)
-
-@validate_data(attribute_model=ProductAttributeModel)
-@use_repository(model=Product)
+@use_repository(repository=ProductRepository)
 async def update_product(
     request: Request, 
-    repository:Repository, 
-    params: Dict[str, Any]
+    repository: ProductRepository, 
     ) -> Response:
 
     try:
         id: UUID = request.path_params["id"]
+        payload = await request.json()
+        schema = ProductSchema()
+        schema.load(payload)
+        params = payload["data"]["attributes"]
+
         model: Model | Product = await repository.update(id=id, params=params)
-        result:  Dict[str, Any] = await repository.serialize_model(model=model)
+        content = schema.dump(obj=model)
 
-    except DoesNotExist as err:
-        logger.exception(err)
-        raise HTTPException(status_code=404, detail=str(err))
+    except Exception as err:
+        if isinstance(err, DoesNotExist):
+            raise HTTPException(status_code=404, detail="Product does not exists")
+        logger.exception(err.args)
+        raise HTTPException(status_code=404, detail=str(err.args))
 
-    return JSONResponse(content=result, status_code=200)
+    return JSONResponse(content=content, status_code=200)
 
-@use_repository(model=Product)
-async def delete_product(request: Request, repository: Repository) -> Response:
+@use_repository(repository=ProductRepository)
+async def delete_product(request: Request, repository: ProductRepository) -> Response:
     try:
         id: UUID = request.path_params["id"]
         await repository.delete(id)
     
     except DoesNotExist as err:
         logger.exception(err)
-        raise HTTPException(status_code=404, detail=str(err))
+        raise HTTPException(status_code=404, detail="Product does not exist")
 
     return Response(status_code=204)
 
@@ -108,7 +112,7 @@ async def delete_product(request: Request, repository: Repository) -> Response:
 routes = [
     Route(path="/", endpoint=get_products, methods=["GET"]),
     Route(path="/", endpoint=create_product, methods=["POST"]),
-    Route(path="/{id:uuid}", endpoint=get_by_id, methods=["GET"]),
+    Route(path="/{id:uuid}", endpoint=get_product_by_id, methods=["GET"]),
     Route(path="/{id:uuid}", endpoint=update_product, methods=["PATCH"]),
     Route(path="/{id:uuid}", endpoint=delete_product, methods=["DELETE"])
 ]
