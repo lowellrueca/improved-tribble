8e0d835bfefceff4ee9b192044dbcd06ab2984c9
lowellrueca <lowellrueca@gmail.com>
2022-07-20 10:18:35 +0800

    add try except clause and use DB_URL var

--- api/events.py
+++ api/events.py
@@ -1,13 +1,24 @@
+import logging
 from tortoise import Tortoise
+from tortoise.exceptions import OperationalError
+from starlette.exceptions import HTTPException
 
+logger: logging.Logger = logging.getLogger("root")
 
 async def on_startup():
-    from .settings import DB_CONFIG
+    from .settings import DB_URL
     from .db import seed_data    
 
-    await Tortoise.init(config=DB_CONFIG)
-    await Tortoise.generate_schemas()
-    await seed_data()
+    try:
+        logger.info("Initializing DB")
+        #await Tortoise.init(config=DB_CONFIG)
+        await Tortoise.init(db_url=DB_URL, modules={"models": ["api.db"]})
+        await Tortoise.generate_schemas()
+        await seed_data()
+    
+    except OperationalError as exc:
+        logger.exception(f"An exception occured: {exc}")
+        raise HTTPException(status_code=500, detail="An internal server error occured")
 
 async def on_shutdown():
     await Tortoise.close_connections()
