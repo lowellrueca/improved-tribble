fea21a04b1992bb2f9c4a9a14983657b365f950a
lowellrueca <lowellrueca@gmail.com>
2022-07-20 10:19:44 +0800

    code simplified

--- api/settings.py
+++ api/settings.py
@@ -1,53 +1,10 @@
 from os import path
 from starlette.config import Config
-from starlette.datastructures import Secret
-from starlette.config import Config
 
 
-env_file = path.abspath(".env")
-config = Config(env_file=env_file)
+config = Config(env_file=path.abspath("envs/settings.env"))
 
-DB_PROV = config(key="DB_PROV", cast=str, default="sqlite")
-DB_USER = config(key="DB_USER", cast=str, default="user")
-DB_PSWD = config(key="DB_PSWD", cast=Secret, default="1234")
-DB_HOST = config(key="DB_HOST", cast=str, default="localhost")
-DB_SERV = config(key="DB_SERV", cast=str, default="localhost")
-DB_PORT = config(key="DB_PORT", cast=int, default=5678)
-DB_NAME = config(key="DB_NAME", cast=str, default="db")
+DB_URL = config(key="DB_URL", cast=str, default="sqlite")
 DEBUG = config(key="DEBUG", cast=bool, default=True)
-HOST = config(key="HOST", cast=str, default="localhost")
+HOST = config(key="HOST", cast=str, default="0.0.0.0")
 PORT = config(key="PORT", cast=int, default=8000)
-
-
-def set_db_url(
-    provider: str, user: str, password: str, host: str, port: int, db_name: str
-) -> str:
-    return f"{provider}://{user}:{password}@{host}:{port}/{db_name}"
-
-DB_CONFIG = {
-    "connections": {
-        "default": set_db_url(
-            DB_PROV,
-            DB_USER,
-            str(DB_PSWD),
-            DB_SERV,
-            DB_PORT,
-            DB_NAME,
-        ),
-    },
-    "apps": {"models": {"models": ["api.db"]}},
-}
-
-AERICH_CONFIG = {
-    "connections": {
-        "default": set_db_url(
-            DB_PROV,
-            DB_USER,
-            str(DB_PSWD),
-            DB_HOST,
-            DB_PORT,
-            DB_NAME,
-        ),
-    },
-    "apps": {"models": {"models": ["api.db", "aerich.models"]}},
-}
