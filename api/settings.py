96ac0629bf667ca44a1ae96ff4e69048fdffaabb
lowellrueca <lowellrueca@gmail.com>
2022-07-20 21:05:13 +0800

    fix format for default db url

--- api/settings.py
+++ api/settings.py
@@ -4,7 +4,7 @@ from starlette.config import Config
 
 config = Config(env_file=path.abspath("envs/settings.env"))
 
-DB_URL = config(key="DB_URL", cast=str, default="sqlite")
+DB_URL = config(key="DB_URL", cast=str, default="sqlite://:memory:")
 DEBUG = config(key="DEBUG", cast=bool, default=True)
 HOST = config(key="HOST", cast=str, default="0.0.0.0")
 PORT = config(key="PORT", cast=int, default=8000)
